63df8f83841c3dc46566d0ff8e9319424fb4fa27
Dzirt232 <dzirt232@rambler.ru>
2017-08-28 23:26:52 +0300

    All created

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
 $(document).ready(function(){
-	for (var i = 0; i < 16; i++) {
-		for (var j = 0; j < 16; j++) {
+	sketcher(16);
+});
+
+function button_click(){
+	$(".block").remove();
+	razmer = prompt("Enter please size of new sketcher",16);
+	sketcher(razmer);
+};
+
+function sketcher(razmer){
+	i_max=razmer;
+	j_max=razmer;
+	for (var i = 0; i < i_max; i++) {
+		for (var j = 0; j < j_max; j++) {
 			$("#Sketcher").append("<div class='block'></div>");
 		}
-	}
-});
+	};
+	if (razmer == 16) {
+		$(".block").height(32);
+		$(".block").width(32);
+	} else {
+		$(".block").height(512/razmer);
+		$(".block").width(512/razmer);	
+	};
+	$( ".block" ).on( "mouseenter", function() {
+		r=Math.floor(Math.random() * (256));
+		g=Math.floor(Math.random() * (256));
+		b=Math.floor(Math.random() * (256));
+		rgbColor = 'rgb('+r+','+g+','+b+')';
+    $( this ).css("background-color",rgbColor);
+  });
+};
+
+
